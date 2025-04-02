c6b809d10cc962d0b5431a90aea2e87453ad5f1d
Mirshod Makhsudov <mirshodmakhsudov@gmail.com>
2025-04-02 05:43:00 +0000

    updatem function ptototypes and datatype in esl_gpio module

--- esl_gpio.c
+++ esl_gpio.c
@@ -1,32 +1,32 @@
 #include "esl_gpio.h"
 
 // Configure pins for LEDs and switch
-void cfg_pins() {
-    nrf_gpio_cfg_output(LED1);
-    nrf_gpio_cfg_output(LED_R);
-    nrf_gpio_cfg_output(LED_G);
-    nrf_gpio_cfg_output(LED_B);
-    nrf_gpio_cfg_input(SW1, NRF_GPIO_PIN_PULLUP);
+void esl_cfg_pins() {
+    nrf_gpio_cfg_output(ESL_IO_PIN_LED1);
+    nrf_gpio_cfg_output(ESL_IO_PIN_LED_R);
+    nrf_gpio_cfg_output(ESL_IO_PIN_LED_G);
+    nrf_gpio_cfg_output(ESL_IO_PIN_LED_B);
+    nrf_gpio_cfg_input(ESL_IO_PIN_SW1, NRF_GPIO_PIN_PULLUP);
 }
 
 // Turn off all LEDs
-void led_off_all() {
-    nrf_gpio_pin_write(LED1, 1);
-    nrf_gpio_pin_write(LED_R, 1);
-    nrf_gpio_pin_write(LED_G, 1);
-    nrf_gpio_pin_write(LED_B, 1);
+void esl_led_off_all() {
+    nrf_gpio_pin_write(ESL_IO_PIN_LED1, 1);
+    nrf_gpio_pin_write(ESL_IO_PIN_LED_R, 1);
+    nrf_gpio_pin_write(ESL_IO_PIN_LED_G, 1);
+    nrf_gpio_pin_write(ESL_IO_PIN_LED_B, 1);
 }
 
 // Turn on a specified LED
-void led_on(esl_io_pin_t pin) {
+void esl_led_on(esl_io_pin_t pin) {
     nrf_gpio_pin_write(pin, 0);
 }
 
 // Turn off a specified LED
-void led_off(esl_io_pin_t pin) {
+void esl_led_off(esl_io_pin_t pin) {
     nrf_gpio_pin_write(pin, 1);
 }
 
-bool btn_is_pressed(void) {
+bool esl_btn_is_pressed(void) {
     return !nrf_gpio_pin_read(SW1);
 }
